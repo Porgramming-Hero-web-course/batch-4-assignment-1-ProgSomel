e4cb471abf0bc61810dd75d2e8feab11f1d70ccd
ProgSomel <somelahmed55@gmail.com>
2024-11-08 15:44:40 +0600

    problem 6 done

---
+++ problem-6.ts
@@ -0,0 +1,15 @@
+{
+    interface Profile {
+        name:string;
+        age:number;
+        email:string;
+    }
+
+    const updateProfile = (obj:Profile, updates: {[k in keyof Profile]?: Profile[k]}):Profile => {
+        return {...obj , ...updates };
+    }
+
+    const myProfile = { name: "Alice", age: 25, email: "alice@example.com" };
+    console.log(updateProfile(myProfile, { age: 26 }));
+
+}
